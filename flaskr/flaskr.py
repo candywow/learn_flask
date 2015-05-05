51fc9c8ba231b00cc4ec11053d576828b5806dea
jieyi <jiezhen@sjtu.edu.cn>
2015-05-05 18:53:25 +0800

    add register

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -20,10 +20,11 @@ def connect_db():
 	return rv
 
 def init_db():
-	db = get_db()
-	with app.open_resource('schema.sql', mode='r') as f:
-		db.cursor().executescript(f.read())
-	db.commit()
+	with app.app_context():
+		db = get_db()
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
 
 def get_db():
 	if not hasattr(g, 'sqlite_db'):
@@ -38,38 +39,62 @@ def close_db(error):
 @app.route('/')
 def show_entries():
 	db = get_db()
-	cur = db.execute('select title, text from entries order by id desc')
+	cur = db.execute('select title, text from article order by id desc')
 	entries = cur.fetchall()
 	return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-	if not session.get('logged_in'):
+	if not session.get('username'):
 		abort(401)
 	db = get_db()
-	db.execute('insert into entries (title, text) values (?, ?)',
-		[request.form['title'], request.form['text']])
+	cur = db.execute('select * from user where name=?', [session['username']])
+	user = cur.fetchone()
+	print user
+	db.execute('insert into article (title, text, author_id) values (?, ?, ?)',
+		[request.form['title'], request.form['text'], user[0]])
 	db.commit()
 	flash('New entry was successfully posted')
 	return redirect(url_for('show_entries'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	error = None
+	if request.method == 'POST':
+		db = get_db()
+		cur = db.execute('select * from user where name=?', [request.form['username']])
+		user = cur.fetchone()
+		if not user: 
+			db.execute('insert into user (name, password) values(?, ?)', 
+				[request.form['username'], request.form['password']])
+			db.commit()
+			flash('Registered')
+			session['username'] = request.form['username']
+			return redirect(url_for('show_entries'))
+		else:
+			flash('The username has already registered!')
+	return render_template('register.html', error=error)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	error = None
 	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME']:
+		db = get_db()
+		cur = db.execute('select * from user where name=?', [request.form['username']])
+		user = cur.fetchone()
+		if not user:
 			error = 'Invalid username'
-		elif request.form['password'] != app.config['PASSWORD']:
+		elif user[2] != request.form['password']:
 			error = 'Invalid password'
 		else:
-			session['logged_in'] = True
+			session['username'] = request.form['username']
 			flash('You are logged in')
 			return redirect(url_for('show_entries'))
 	return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
-	session.pop('logged_in', None)
+	session.pop('username', None)
 	flash('You were logged out')
 	return redirect(url_for('show_entries'))
 
--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,6 +1,21 @@
-drop table if exists entries;
-create table entries (
+drop table if exists user;
+drop table if exists article;
+--
+--  user
+--
+create table user (
+	id integer primary key autoincrement,
+	name string not null,
+	password string not null
+);
+
+--
+-- article
+--
+create table article (
 	id integer primary key autoincrement,
 	title string not null,
-	text string not null
+	text strinnot null,
+	author_id integer,
+	foreign key(author_id) references user(id)
 );
