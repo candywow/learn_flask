712c9bf74980c932f12c25f6c1e5e86f42f1ec91
jieyi <jiezhen@sjtu.edu.cn>
2015-05-07 21:04:04 +0800

    修改:         flasky/hello.py 	新文件:       flasky/templates/base.html 	新文件:       flasky/templates/index.html 	新文件:       flasky/templates/user.html

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,16 +1,16 @@
-from flask import Flask,request
-from flask.ext.script import Manager
+from flask import Flask, request, render_template
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
-manager = Manager(app)
+bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/index')
 def index():
-	return '<h1>Bad Request</h1>', 400
+	return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>Hello, %s!</h1>' % name
+	return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-	manager.run()
+	app.run(debug=True)
