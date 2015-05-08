fb6e2e310e4f14076174779ea9ec398ea75f1a44
jieyi <jiezhen@sjtu.edu.cn>
2015-05-08 12:19:28 +0800

    修改:         flasky/hello.py 	修改:         flasky/templates/base.html 	修改:         flasky/templates/index.html

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,12 +1,28 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-@app.route('/')
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[Required()])
+	submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	form = NameForm()
+	if form.validate_on_submit(): #The validate_on_submit() method of the form return True when the form was submitted and the data has been accpted by all the field validators. 
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name'] = form.name.data
+		form.name.data = ' '
+		return redirect(url_for('index')) #A redirect is a special type of response that has a URL instead of a string with HTML code
+	return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
